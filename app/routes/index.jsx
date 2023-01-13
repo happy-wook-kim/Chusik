b80562a523a6b7e4c23a5e8a3d1d3be3f3e6d10d
jinwook <catdog6210@gmail.com>
2023-01-13 13:47:54 +0900

    main view changing

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,12 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+      <Link prefetch="intent" to="/restaurant">음식점</Link>
+      <Link prefetch="intent" to="/restaurant/detail">음식점 상세</Link>
     </div>
   );
 }
