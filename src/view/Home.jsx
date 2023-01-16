ff563744367469e98a943218d4253b43d1f69219
jinwook <catdog6210@gmail.com>
2023-01-16 10:22:08 +0900

    fisrt commit

---
+++ src/view/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+
+export default function Home() {
+
+
+  return(
+    <div>
+      <h1>HOME!</h1>
+      <Link to="/test">TEST</Link>
+    </div>
+  )
+}
