a607aa8d48b0af244d09e17d0bbe005623fe8320
jinwook <catdog6210@gmail.com>
2023-01-16 17:49:46 +0900

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './scss/normalize.scss'  
+import NotFound from './layout/NotFound'
+import Home from './view/Home'
+import Restaurants from './view/restaurants'
+import BasicLayout from './layout/BasicLayout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<BasicLayout />}>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/restaurants'} element={<Restaurants />} />
+        </Route>
+        <Route path={'/*'} element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
