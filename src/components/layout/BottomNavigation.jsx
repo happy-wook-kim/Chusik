b2050ab77fe0587ce2165660191927074a9d6a8b
jinwook <catdog6210@gmail.com>
2023-01-17 15:39:51 +0900

    add button and svg

--- src/components/layout/BottomNavigation.jsx
+++ src/components/layout/BottomNavigation.jsx
@@ -1,19 +1,43 @@
 import styles from "./BottomNavigation.module.scss"
+import { useState } from "react"
 
 export default function BottomNavigation() {
+  const [navi, setNavi] = useState([
+    {
+      index: 0,
+      value: true,
+      title: '식당 찾기',
+      src: new URL('../../assets/map.svg', import.meta.url).href
+    },
+    {
+      index: 1,
+      value: false,
+      title: '즐겨찾기',
+      src: new URL('../../assets/bookmark.svg', import.meta.url).href
+    },
+    {
+      index: 2,
+      value: false,
+      title: '설정',
+      src: new URL('../../assets/settings.svg', import.meta.url).href
+    }
+  ])
 
+  const onClick = (a) => {
+    console.log('13123')
+    console.log(a.target)
+  }
 
   return (
     <div className={styles.navigation}>
-      <section>
-        <span>식당 찾기</span>
-      </section>
-      <section>
-        <span>카테고리</span>
-      </section>
-      <section>
-        <span>설정</span>
-      </section>
+      {navi?.map((item, i) => 
+        <section key={item.index}>
+          <button className={item.value ? 'active' : 'none'} onClick={onClick}>
+            <img src={item.src} />
+            {item.title}
+          </button>
+        </section>
+      )}
     </div>
   )
 }
