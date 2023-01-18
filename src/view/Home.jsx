d95f060d98efd97b047a24b7826996ce392ceeb0
jinwook <catdog6210@gmail.com>
2023-01-18 16:21:10 +0900

    add redirect

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
-import styles from "./Home.module.scss"
-
+import { Link, Navigate } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 export default function Home() {
 
   return(
     <div>
-      <h1>HELLO THIS IS CHU SIK</h1>
+      {/* <h1>HELLO THIS IS CHU SIK</h1>
       <Link to="/restaurants">
         <button>Go</button>
       </Link>
       <Link to="/restaurant">
         <button>Go 404</button>
-      </Link>
+      </Link> */}
+      <Navigate to="/restaurants" />
     </div>
   )
 }
