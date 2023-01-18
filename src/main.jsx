1f86497f85b7d1f64d963438d7f702283ed63eb9
jinwook <catdog6210@gmail.com>
2023-01-18 16:21:17 +0900

    add pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import NotFound from './layout/NotFound'
 import Home from './view/Home'
 import Restaurants from './view/restaurants'
 import BasicLayout from './layout/BasicLayout'
+import Settings from './view/settings'
+import Favorites from './view/favorites'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +17,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path={'/'} element={<BasicLayout />}>
           <Route path={'/'} element={<Home />} />
           <Route path={'/restaurants'} element={<Restaurants />} />
+          <Route path={'/favorites'} element={<Favorites />} />
+          <Route path={'/settings'} element={<Settings />} />
           <Route path={'/*'} element={<NotFound />} />
         </Route>
       </Routes>
