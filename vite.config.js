17bc965c617955c41bb6bba4d20a5ceb46743455
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2023-03-16 10:12:40 +0900

    change library version

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
-react({ plugins: [["@swc/plugin-styled-components", {}]] });
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
