8f031c72c6bb77de5023d2bf5f97dcacbef25a50
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2023-03-20 13:42:08 +0900

    set proxy to protect api url

--- src/view/restaurants/search.jsx
+++ src/view/restaurants/search.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react"
 import styles from "./search.module.scss"
 
 export default function search() {
+  const [searchText, setText] = useState(1)
+  const [myObject, setObject] = useState({
+    id: '',
+    title: ''
+  })
 
+  const textSearch = (e) => {
+    setText(() => e.target.value)
+  }
+
+  const search = () => {
+    fetch(`/api/posts/${searchText}`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        setObject(() => {
+          return {
+            ...json
+          }
+        })
+      });
+  }
 
   return (
     <div>
       <h1>search!</h1>
+      <input value={searchText} 
+        onInput={textSearch}/>
+      <button onClick={search}></button>
+      {searchText.length > 0 &&
+      <div>
+        <span>"{searchText}" 검색 목록</span>
+      </div>
+      }
+      {myObject.title}
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+
+process.env = {...process.env, ...loadEnv('', process.cwd())}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8470
+    port: 8470,
+    proxy: {
+      '/api' : {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/api', '/')
+      }
+    }
   },
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src")}
     ]
-  }
+  },
 })
