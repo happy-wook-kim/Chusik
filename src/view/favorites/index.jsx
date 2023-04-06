e1e0566cd6d4705f76750b9c782e5462fb889960
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2023-04-06 18:39:52 +0900

    add dummy data

--- src/view/favorites/index.jsx
+++ src/view/favorites/index.jsx
@@ -1,11 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useMemo } from "react"
 import styles from "./favorite.module.scss"
 
 export default function Favorites() {
+  const data = [
+    {title: '일식당', writer: '1번', icon: '1.png',},
+    {title: '이이식당', writer: '2번', icon: '2.png',},
+    {title: '사사삼식당', writer: '3번', icon: '3.png',},
+  ]
+
+  const restaurantData = useMemo(() => {
+    return {
+      title: data.title
+    }
+  }, [data])
+
+  useEffect(() => {
+    console.log(restaurantData)
+  })
 
   return (
     <div>
-      <h1>favorite!</h1>
+      <h1>나만의 식당</h1>
+      {restaurantData.title}
     </div>
   )
 }
